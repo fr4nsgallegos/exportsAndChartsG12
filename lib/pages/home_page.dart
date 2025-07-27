b82e44e3cab8ccd6423f35d8201416795e23042e
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-27 10:40:08 -0500

    generar pdf con imágenges

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path/path.dart' as path;
 
@@ -79,6 +81,51 @@ class HomePage extends StatelessWidget {
     return file;
   }
 
+  Future<File> generarPdfConImagen() async {
+    final pdf = pw.Document();
+    final image = pw.MemoryImage(
+      (await rootBundle.load("assets/images/peru.jpeg")).buffer.asUint8List(),
+    );
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Column(
+              children: [
+                pw.Text(
+                  "PDF CON IMÁGEN",
+                  style: pw.TextStyle(
+                    fontSize: 24,
+                    fontWeight: pw.FontWeight.bold,
+                  ),
+                ),
+                pw.SizedBox(height: 16),
+                pw.Text(
+                  "Este es un ejemplo de parrafo para el pdf creado con imágen4es",
+                  style: pw.TextStyle(color: PdfColors.blue),
+                ),
+                pw.SizedBox(height: 32),
+                pw.Image(image, width: 200, height: 200, fit: pw.BoxFit.cover),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+    // Obtener la ruta de almacenamiento local
+    final output = await getApplicationDocumentsDirectory();
+
+    // usando path para generar correctamente la ruta
+    final filePath = path.join(output.path, "exampleWithImage.pdf");
+    final file = File(filePath);
+
+    // Guardar el archivo
+    await file.writeAsBytes(await pdf.save());
+    print("PDF guardado en ${file.path}");
+    return file;
+  }
+
   void openPdfFile(File filePdf) async {
     try {
       print("Intentando abrir el pdf");
@@ -106,6 +153,10 @@ class HomePage extends StatelessWidget {
             }),
             _buildButton("Generar PDF con tabla", () async {
               final pdfFile = await generarTablePdf();
+              openPdfFile(pdfFile);
+            }),
+            _buildButton("Generar PDF con imágen", () async {
+              final pdfFile = await generarPdfConImagen();
               openPdfFile(pdfFile);
             }),
           ],
