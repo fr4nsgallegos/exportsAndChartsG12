a93e08874cc486b25c6a6586cd45fcd85f4924df
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-27 09:27:16 -0500

    firste

---
+++ android/app/src/main/kotlin/com/g12/exportsandchartsg12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.g12.exportsandchartsg12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:exportsandchartsg12/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
